ef7b845349b35235979363b8a5b03282ac3445d0
André Frisk <50335484+Stronkness@users.noreply.github.com>
2022-02-22 23:22:29 +0100

    Delete EDAF45 - Programvaruutveckling i grupp directory

--- EDAF45 - Programvaruutveckling i grupp/Team 12/result/src/main/java/result/varvlopp/LapResult.java
+++
@@ -1,9 +0,0 @@
-package result.varvlopp;
-
-import result.Result;
-
-public interface LapResult extends Result {
-    String getLap(int lap);
-    int getNumberOfLaps();
-    String getVarvning(int maxLaps);
-}
