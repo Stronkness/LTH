5c3d62b1212e6d734c17bd554eb143ab18d68e85
André Frisk <50335484+Stronkness@users.noreply.github.com>
2022-02-22 23:15:26 +0100

    Delete EDAP10 - Flertrådad Programmering directory

--- EDAP10 - Flertrådad Programmering/chat/hello/hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int
-main()
-{
-  printf("Congratulations! Your computer is set up to\n"
-         "compile and run C programs from the command line.\n");
-}
--- EDAP10 - Flertrådad Programmering/widgets/src/train/model/Route.java
+++
@@ -1,8 +0,0 @@
-package train.model;
-
-/** A train route: a never-ending sequence of track segments. */
-public interface Route {
-    
-    /** Returns the next segment in this route. */
-    Segment next();
-}
